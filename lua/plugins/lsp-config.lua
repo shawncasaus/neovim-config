dac63d09d122cb59eca0ee2244e732715fd02990
shawncasaus <shawn.casaus@gmail.com>
2024-02-14 17:02:52 +0000

    Added live language server

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,33 @@
+return {
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = { "lua_ls", "tsserver", "prismals", "java_language_server", "html", "eslint", "cssls" }
+            })
+        end
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            local lspconfig = require('lspconfig')
+            lspconfig.lua_ls.setup({})
+            lspconfig.tsserver.setup({})
+            lspconfig.prismals.setup({})
+            lspconfig.java_language_server.setup({})
+            lspconfig.html.setup({})
+            lspconfig.eslint.setup({})
+            lspconfig.cssls.setup({})
+
+            vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+            vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
+        end
+    }
+}

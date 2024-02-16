d3096d5c8d2be565565ed334cec10354df663568
shawncasaus <shawn.casaus@gmail.com>
2024-02-16 23:50:31 +0000

    Added Cpp support

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -24,6 +24,7 @@ return {
 			lspconfig.html.setup({ capabilities = capabilities })
 			lspconfig.eslint.setup({ capabilities = capabilities })
 			lspconfig.cssls.setup({ capabilities = capabilities })
+			lspconfig.clangd.setup({ capabilities = capabilities })
 
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})

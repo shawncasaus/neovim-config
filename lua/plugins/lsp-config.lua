4709d5200c8393726288633c70eb860bfa671d12
shawncasaus <shawn.casaus@gmail.com>
2024-02-16 15:16:48 +0000

    Added pyright language server

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
         "williamboman/mason-lspconfig.nvim",
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "tsserver", "prismals", "html", "eslint", "cssls" }
+                ensure_installed = { "lua_ls", "tsserver", "prismals", "html", "eslint", "cssls", "pyright" }
             })
         end
     },
